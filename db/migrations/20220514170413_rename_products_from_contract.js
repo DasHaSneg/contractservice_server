d7004941d9874a0c794d6859dcbe0af13cc03490
Daria Sneg <dashasneg@mail.ru>
2022-05-14 22:07:12 +0500

    delete json src from tables

---
+++ db/migrations/20220514170413_rename_products_from_contract.js
@@ -0,0 +1,25 @@
+exports.up = async knex => {
+    await knex.schema
+    .table('contract', t => {
+        t.dropColumn('products');
+        t.jsonb('file').notNull();
+    })
+    .then(() => console.log('droped created annex src'))
+    .catch(err => {
+        console.log(err)
+        throw err
+    })
+};
+  
+exports.down = async knex => {
+    await knex.schema
+    .table('contract', t =>  {
+        t.jsonb('products').notNull();
+        t.dropColumn('file');
+    })
+    .then(() => console.log('created annex src'))
+    .catch(err => {
+        console.log(err)
+        throw err
+    })
+}
